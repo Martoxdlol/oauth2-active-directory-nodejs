72b38e454b1f2ab171a05308e8250b4983975e00
Martoxdlol <martoxdlol@gmail.com>
2021-11-25 08:37:20 -0300

    REDIS_URL -> REDIS_URI

--- backend/src/redis.js
+++ backend/src/redis.js
@@ -1,5 +1,5 @@
 const asyncRedis = require('async-redis')
 const redis = require("redis")
-const client = redis.createClient(process.env.REDIS_URL ? process.env.REDIS_URL : undefined)
+const client = redis.createClient(process.env.REDIS_URI)
 module.exports.callback_client = client
 module.exports.redisClient = asyncRedis.decorate(client)
