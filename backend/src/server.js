5ab08e48fd3b7b19f080dea117d4a9ed70ff0430
Martoxdlol <martoxdlol@gmail.com>
2021-11-25 09:46:18 -0300

    en changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,17 @@ console.log("************************* ENV *************************")
 console.log(process.env)
 console.log("*******************************************************")
 
+const prod = process.env.NODE_ENV == 'production'
+
+// Use only on development
+if (!prod && !process.env.AUTH_SERVER_HOST) process.env.AUTH_SERVER_HOST = 'localhost:8080'
+if (!prod && !process.env.ALLOW_KEEP_LOGGED_USERS) process.env.ALLOW_KEEP_LOGGED_USERS = 'true'
+if (!prod && !process.env.MONGO_URI) process.env.MONGO_URI = 'mongodb://localhost:27017/auth-server'
+if (!process.env.REDIS_PREFIX) process.env.REDIS_PREFIX = ''
+if (!process.env.ACCESS_TOKEN_DURATION) process.env.ACCESS_TOKEN_DURATION = 10 * 60
+if (!process.env.RESET_PASSWORD_LINK_DURATION) process.env.RESET_PASSWORD_LINK_DURATION = 10 * 60
+process.env.ACCESS_TOKEN_DURATION = parseInt(process.env.ACCESS_TOKEN_DURATION)
+process.env.RESET_PASSWORD_LINK_DURATION = parseInt(process.env.RESET_PASSWORD_LINK_DURATION)
 
 const express = require("express")
 const path = require('path')
@@ -12,16 +23,7 @@ const db = require("./db")
 const oauth2Router = require("./oauth2/oauth2")
 const createAndSetSession = require("./session")
 
-const prod = process.env.NODE_ENV == 'production'
 const WORKDIR_PATH = process.env.WORKDIR_PATH
-
-
-// Use only on development
-if (!prod && !process.env.AUTH_SERVER_HOST) process.env.AUTH_SERVER_HOST = 'localhost:8080'
-if (!prod && !process.env.ALLOW_KEEP_LOGGED_USERS) process.env.ALLOW_KEEP_LOGGED_USERS = 'true'
-if (!process.env.REDIS_PREFIX) process.env.REDIS_PREFIX = ''
-if (!process.env.ACCESS_TOKEN_DURATION) process.env.ACCESS_TOKEN_DURATION = 10 * 60
-if (!process.env.RESET_PASSWORD_LINK_DURATION) process.env.RESET_PASSWORD_LINK_DURATION = 10 * 60
 
 
 async function main() {
@@ -65,4 +67,4 @@ async function main() {
     console.log("Auth server listening on port 8080");
 }
 
-main()
+main().catch(e => { throw e })
